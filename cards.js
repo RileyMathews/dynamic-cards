f98a2d15bcb12a87e1804e021b35c0c682b6ab52
Riley Mathews <rileymathews80@gmail.com>
2018-04-25 08:56:32 -0500

    added incrimenting id's to cards and buttons

--- cards.js
+++ cards.js
@@ -1,9 +1,13 @@
 //app to create a card element for the dom
+let currentCard = 1
 const buildCard = (text) => {
     const card = document.createElement("card")
     card.textContent = text
+    card.setAttribute("id", "card--"+currentCard)
+    currentCard++
     const button = document.createElement("button")
     button.textContent = "delete"
+    button.setAttribute("id", "button--"+currentCard)
     card.appendChild(button)
     return card
 }
