36bb5aa64717937c4faabd83785ba8858770dc0f
Riley Mathews <rileymathews80@gmail.com>
2018-04-25 08:52:14 -0500

    added functionality to create button

--- print.js
+++ print.js
@@ -1,7 +1,6 @@
 //app to print cards to the dom
-const printCard = (text) => {
+const printCard = () => {
     const output = document.querySelector("#output")
+    const text = document.querySelector("#text-input").value
     output.appendChild(buildCard(text))
 }
-
-printCard("test")
---
+++ submit.js
@@ -0,0 +1,4 @@
+//app to handle when user clicks the submit button
+const createButton = document.querySelector("#create")
+const textContent = document.querySelector("#text-input")
+createButton.addEventListener("click", printCard)
